cbef00f77f022c9be98d9a304cc9c917e97cc9fa
Ibrahim Tareq <ibrahim.tareq@news.com.au>
2019-10-29 20:46:39 +1100

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from 'react'
+
+import { SearchBar, VideoDetail } from './components'
+import { Grid } from '@material-ui/core'
+import Youtube from './api/youtube'
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Test</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            <SearchBar/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail/>
+                        </Grid>
+                        <Grid item xs={4}>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
